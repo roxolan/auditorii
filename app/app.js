c99dcc892dd14fe616ba7f47e241ac981fbbaf33
roxolan <roxolanus@gmail.com>
2015-06-02 07:08:10 +0300

    Initial Classrooms Reservation page

--- app/app.js
+++ app/app.js
@@ -1,1 +1,16 @@
-angular.module('auditorii', []);
+angular.module('auditorii', []).
+
+  controller('roomCtrl', function ($scope) {
+    $scope.rooms = [
+      {number: '432A'},
+      {number: '428B'},
+      {number: '416C'}
+    ];
+
+    // reserving the room
+    $scope.reserve = function (room) {
+      $scope.rooms.push(angular.copy(room));
+      delete $scope.room;
+    }
+  });
+
