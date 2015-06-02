e12c8931efcfd0a78d76e2c2ced55f153686d18e
roxolan <roxolanus@gmail.com>
2015-06-02 10:32:24 +0300

    Add curators to the view

--- app/app.js
+++ app/app.js
@@ -5,10 +5,19 @@ angular.module('auditorii', [
   controller('roomCtrl', function ($scope) {
     $scope.appTitle = "<b>Classrooms Reservation</b>";
 
+    // List of rooms
     $scope.rooms = [
       {number: '432A', title: 'Picasso room'},
       {number: '428B', title: 'Hegel room'},
       {number: '416C', title: 'Humboldt room'}
+    ];
+
+    // List of room curators
+    $scope.curators = [
+      "Mary Johnson",
+      "Gary Donovan",
+      "Kira Lembrooq",
+      "Lisa Dreary"
     ];
 
     // reserving the room
