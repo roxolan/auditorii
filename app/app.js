a05731bef7978f4988bcc8943dc8d1a23e22cd45
roxolan <roxolanus@gmail.com>
2015-06-02 09:49:33 +0300

    Add style file, input checkbox to select rooms

--- app/app.js
+++ app/app.js
@@ -6,13 +6,14 @@ angular.module('auditorii', [
     $scope.appTitle = "<b>Classrooms Reservation</b>";
 
     $scope.rooms = [
-      {number: '432A'},
-      {number: '428B'},
-      {number: '416C'}
+      {number: '432A', title: 'Picasso room'},
+      {number: '428B', title: 'Hegel room'},
+      {number: '416C', title: 'Humboldt room'}
     ];
 
     // reserving the room
     $scope.reserve = function (room) {
+      room.startTime = new Date();
       $scope.rooms.push(angular.copy(room));
       delete $scope.room;
     }
