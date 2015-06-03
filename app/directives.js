07c5587af2c75608cdd1b52c67e2a7b9d9333a6f
roxolan <roxolanus@gmail.com>
2015-06-03 06:55:46 +0300

    Passing data in a directive as strings

--- app/directives.js
+++ app/directives.js
@@ -1,6 +1,11 @@
 auditorii.directive("alert", function () {
   return {
+    restrict: 'E',
+    scope: {
+      topic: '@topic',
+      description: '@description'
+    },
     templateUrl: "templates/alert.html",
     replace: true
-  }
+  };
 });
